ca1dc4cc603710b666d51fd7f00d29589a0d4242
COLLINS <colloroyce@gmail.com>
2023-03-19 10:47:38 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/* Dropdown menu */
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
+  
+  /* Slideshow gallery */
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var slides = document.getElementsByClassName("mySlides");
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+  
+  /* Contact form validation */
+  function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    if (name == "" || email == "" || message == "") {
+      alert("Please fill in all fields.");
+      return false;
+    }
+  }
+  
